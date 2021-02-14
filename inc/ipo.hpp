8a817ab3f1309b47757958d9773ff0490e535a38
Sunshine <sunshine@uberspace.net>
2021-02-13 23:26:34 -1000

    switch stock_exchange to QString

--- inc/ipo.hpp
+++ inc/ipo.hpp
@@ -2,24 +2,10 @@
 
 #include <QDateTime>
 
-enum Exchange {
-    UNKNOWN = 0;   // Unknown
-    NYSE    = 1;   // New York Stock Exchange
-    NASDAQ  = 2;   // National Association of Securities Dealers Automated Quotations
-    TSX     = 4;   // Toronto Stock Exchange
-    SEHK    = 8;   // Hong Kong Exchange
-    LSE     = 16;  // London Stock Exchange
-    TSE     = 32;  // Tokyo Stock Exchange
-    SZSE    = 64;  // Shenzhen Stock Exchange
-    TADAWUL = 128; // Saudi Stock Exchange
-    PINK    = 256; // OTC Markets Pink
-    MEXI    = 512; // Mexican Stock Exchange
-};
-
 struct Ipo {
     QString   company_name;
     QDateTime expected_date;
-    Exchange  stock_exchange,
+    QString   stock_exchange,
     QString   ticker;
     QUrl      website;
 };
