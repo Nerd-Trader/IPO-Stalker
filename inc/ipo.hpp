cddc74dfc1df949b520cd0089f4c801185d164e9
Sunshine <sunshine@uberspace.net>
2021-02-13 23:27:42 -1000

    rename website to company_website

--- inc/ipo.hpp
+++ inc/ipo.hpp
@@ -4,8 +4,8 @@
 
 struct Ipo {
     QString   company_name;
+    QUrl      company_website;
     QDateTime expected_date;
     QString   stock_exchange,
     QString   ticker;
-    QUrl      website;
 };
